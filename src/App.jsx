f6e3ad194c99964955040eb50bceb07eb35ac7a1
Abdelrahman Waheed <wabdo5242@gmail.com>
2024-08-05 18:37:15 +0300

    initial project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-
 function App() {
-
-
   return (
     <>
-      <h1 className="bg-red-500"> welcome back</h1>
-      <h1 className="bg-red-500"> abdo waheed</h1>
+    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
