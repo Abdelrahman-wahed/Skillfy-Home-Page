b72dc9b3d04f709851a37e7738ea262f276d8935
Abdelrahman Waheed <wabdo5242@gmail.com>
2024-08-05 18:12:41 +0300

    change first h1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className="bg-red-500"> fyc</h1>
+      <h1 className="bg-red-500"> welcome back</h1>
       <h1 className="bg-red-500"> abdo waheed</h1>
     </>
   )
