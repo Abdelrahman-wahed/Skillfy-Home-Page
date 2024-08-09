dc4d9e70d0ad91c236738c5d3139f9aa8033912e
Abdelrahman Waheed <wabdo5242@gmail.com>
2024-08-10 02:45:32 +0300

    comment some lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Start from "./components/Start";
-import Welcome from "./components/Welcome";
-import Welcome02 from "./components/Welcome02";
-import Welcome03 from "./components/Welcome03";
+// import Start from "./components/Start";
+// import Welcome from "./components/Welcome";
+// import Welcome02 from "./components/Welcome02";
+// import Welcome03 from "./components/Welcome03";
 import Home from "./components/Home";
 function App() {
   return (
